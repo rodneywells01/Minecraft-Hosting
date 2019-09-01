a68854ebf5bbc3f7ab0e9fd7f70a2bc8daf30aad
Rodney Wells <rodneywells01@gmail.com>
2019-09-01 05:32:36 -0400

    Added Discord Bot basic code

---
+++ bot/scripts/push-image.sh
@@ -0,0 +1,2 @@
+docker tag minecraftbot_discord-bot rodneywells01/minecraft-discord-bot:latest
+docker push rodneywells01/minecraft-discord-bot:latest
---
+++ bot/scripts/user-data.sh
@@ -0,0 +1,2 @@
+docker pull rodneywells01/minecraft-discord-bot:latest
+docker run --detach --name minecraft-discord-bot rodneywells01/minecraft-discord-bot:latest
