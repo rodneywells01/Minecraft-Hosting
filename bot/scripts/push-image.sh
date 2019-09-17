f4cc9ffc3d2b897232945eba3f4f18a04f511e76
Rodney Wells <rodneywells01@gmail.com>
2019-09-17 18:57:25 -0400

    Deployment automation improvement

--- bot/scripts/push-image.sh
+++ bot/scripts/push-image.sh
@@ -1,2 +1,2 @@
-docker tag minecraft-hosting_bot rodneywells01/minecraft-discord-bot:latest
+docker build . --tag rodneywells01/minecraft-discord-bot:latest
 docker push rodneywells01/minecraft-discord-bot:latest
--- bot/scripts/user-data.sh
+++ bot/scripts/user-data.sh
@@ -1,8 +1,4 @@
-# Install Docker
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
+#!/bin/bash
 
 # Pull and start bot
 sudo docker pull rodneywells01/minecraft-discord-bot:latest
